14dcae1625fe5e0b2e8946cf6ec9a13775c2c34e
Jurgita <jurgita.eicinaite@yahoo.com>
2023-08-28 11:09:36 +0300

    /api/breeds-page implementation

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const paginationFolderPath = './pagination';
 if (!fs.existsSync(paginationFolderPath)) {
     fs.mkdirSync(paginationFolderPath);
     fs.writeFileSync(`./pagination/breed_list.json`, JSON.stringify([]), 'utf8');
+    fs.writeFileSync(
+        `./pagination/paginationSettings.json`,
+        JSON.stringify((module.exports = { default: 5, min: 2, max: 20 })),
+        'utf8'
+    );
 }
 
 app.post('/api/breed', (req, res) => {
@@ -51,11 +56,25 @@ app.get('/api/breeds/:id', (req, res) => {
     const id = req.params.id;
     let breedListData = fs.readFileSync(`./pagination/breed_list.json`, 'utf8');
     breedListData = JSON.parse(breedListData);
-    if (breedListData.length !== 0 && breedListData.find(breed => id === breed.id)) {
+    if (breedListData.length && breedListData.find(breed => id === breed.id)) {
         res.json(breedListData.find(breed => id === breed.id));
     } else {
         res.status(404).json({ message: 'No such breed found' });
     }
+});
+
+app.get('/api/breeds-page', (req, res) => {
+    const { max, min, def } = JSON.parse(fs.readFileSync('./pagination/paginationSettings.json', 'utf8'));
+    let breedListData = fs.readFileSync(`./pagination/breed_list.json`, 'utf8');
+    breedListData = JSON.parse(breedListData);
+    const sortedBreedList = breedListData.sort((a, b) => {
+        console.log('obj', a, b);
+        if (a.breed === b.breed) {
+            return a.creationTime > b.creationTime ? 1 : -1;
+        }
+        return a.breed.localeCompare(b.breed);
+    });
+    res.json(sortedBreedList);
 });
 
 app.listen(port, () => {
