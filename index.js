c3cc4ef5e1f0338209edf41d2833ea4c0f0ae94a
Jurgita <jurgita.eicinaite@yahoo.com>
2023-08-28 10:15:41 +0300

    refactor initial code

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+console.clear();
 const breedsFolderPath = './breeds';
 if (!fs.existsSync(breedsFolderPath)) {
     fs.mkdirSync(breedsFolderPath);
+}
+const paginationFolderPath = './pagination';
+if (!fs.existsSync(paginationFolderPath)) {
+    fs.mkdirSync(paginationFolderPath);
+    fs.writeFileSync(`./pagination/breed_list.json`, JSON.stringify([]), 'utf8');
 }
 
 app.post('/api/breed', (req, res) => {
@@ -26,25 +32,28 @@ app.post('/api/breed', (req, res) => {
         .split(' ')
         .map((word, i) => (i !== 0 ? word[0].toUpperCase() + word.slice(1) : word))
         .join('')}-${id}`;
+    // Create file
     const filePath = `./breeds/${fileName}.json`;
     fs.writeFileSync(filePath, JSON.stringify(breedObject), 'utf8');
+    // Push new obj to breedList data
+    let breedListData = fs.readFileSync(`./pagination/breed_list.json`, 'utf8');
+    breedListData = JSON.parse(breedListData);
+    breedListData.push(breedObject);
+    fs.writeFileSync(`./pagination/breed_list.json`, JSON.stringify(breedListData), 'utf8');
 
     res.status(201).json({ message: `${breedObject.breed} successfully added.` });
 });
 
 app.get('/api/breeds', (req, res) => {
-    res.json(objectListFromFiles(breedsFolderPath));
+    res.json(JSON.parse(fs.readFileSync(`./pagination/breed_list.json`, 'utf8')));
 });
 
 app.get('/api/breeds/:id', (req, res) => {
     const id = req.params.id;
-
-    const fileList = fs.readdirSync(breedsFolderPath);
-    const fileExists = fileList.find(str => str.includes(id));
-
-    if (fileExists) {
-        const requestedBreed = fs.readFileSync(`./breeds/${fileExists}`);
-        res.json(JSON.parse(requestedBreed));
+    let breedListData = fs.readFileSync(`./pagination/breed_list.json`, 'utf8');
+    breedListData = JSON.parse(breedListData);
+    if (breedListData.length !== 0 && breedListData.find(breed => id === breed.id)) {
+        res.json(breedListData.find(breed => id === breed.id));
     } else {
         res.status(404).json({ message: 'No such breed found' });
     }
